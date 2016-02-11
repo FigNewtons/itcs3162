51a396832c46e3f7085197ea2364fb89215ba11c
Fig Newtons <dev.dangruszczynski@gmail.com>
2016-02-11 15:31:31 -0500

    Homework 1: Pulled 9 out of 10 stories from CNN

---
+++ cnn/scrape.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+root = "http://cnn.com"
+
+home_page = requests.get(root).content
+
+soup = BeautifulSoup(home_page, 'html.parser')
+
+urls = [ link.get("href") for link in soup.select("h2.cn__title ~ li a")[:10]]
+top_stories = [ root + url if url[0] == '/' else url for url in urls]
+
+
+stories = []
+
+for url in top_stories:
+    page = requests.get(url).content
+    page_soup = BeautifulSoup(page, 'html.parser')
+    raw_story = page_soup.find_all("p", class_ = "zn-body__paragraph")[1: -1]
+    stories.append( ' '.join([line.get_text() for line in raw_story]))
+    
+
+for story in stories:
+    print(story + "\n\n\n")
+
+
+
+
